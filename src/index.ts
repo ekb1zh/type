f27940bb3022203d4bb589c07ca4ec3590ac4598
ekb1zh <ekb1zh@gmail.com>
2022-04-13 23:48:58 +0300

    Fix export

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,44 @@
-enum Kind {
-  BOOLEAN = 'BOOLEAN',
-  NUMBER = 'NUMBER',
-  STRING = 'STRING',
-  SYMBOL = 'SYMBOL',
-  BIGINT = 'BIGINT',
-  OBJECT = 'OBJECT',
-  ARRAY = 'ARRAY',
-  FUNCTION = 'FUNCTION',
-  NULL = 'NULL',
-  UNDEFINED = 'UNDEFINED',
-}
+namespace Type {
+  export enum Kind {
+    BOOLEAN = 'BOOLEAN',
+    NUMBER = 'NUMBER',
+    STRING = 'STRING',
+    SYMBOL = 'SYMBOL',
+    BIGINT = 'BIGINT',
+    OBJECT = 'OBJECT',
+    ARRAY = 'ARRAY',
+    FUNCTION = 'FUNCTION',
+    NULL = 'NULL',
+    UNDEFINED = 'UNDEFINED',
+  }
 
-const of = (value: unknown): Kind => {
-  const type = typeof value
-  switch (type) {
-    case 'boolean':
-      return Kind.BOOLEAN
-    case 'number':
-      return Kind.NUMBER
-    case 'string':
-      return Kind.STRING
-    case 'symbol':
-      return Kind.SYMBOL
-    case 'bigint':
-      return Kind.BIGINT
-    case 'undefined':
-      return Kind.UNDEFINED
-    case 'function':
-      return Kind.FUNCTION
-    case 'object':
-      return value === null
-        ? Kind.NULL
-        : Array.isArray(value)
-        ? Kind.ARRAY
-        : Kind.OBJECT
-    default:
-      throw new TypeError(String(value))
+  export const of = (value: unknown): Kind => {
+    const type = typeof value
+    switch (type) {
+      case 'boolean':
+        return Kind.BOOLEAN
+      case 'number':
+        return Kind.NUMBER
+      case 'string':
+        return Kind.STRING
+      case 'symbol':
+        return Kind.SYMBOL
+      case 'bigint':
+        return Kind.BIGINT
+      case 'undefined':
+        return Kind.UNDEFINED
+      case 'function':
+        return Kind.FUNCTION
+      case 'object':
+        return value === null
+          ? Kind.NULL
+          : Array.isArray(value)
+          ? Kind.ARRAY
+          : Kind.OBJECT
+      default:
+        throw new TypeError(String(value))
+    }
   }
 }
 
-export default {
-  Kind,
-  of,
-}
+export default Type
