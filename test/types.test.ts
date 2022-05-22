8b738cd26d6553d71f2d543fbfe50448e0276046
ekb1zh <ekb1zh@gmail.com>
2022-05-22 19:14:25 +0300

    Update tests

--- test/types.test.ts
+++ test/types.test.ts
@@ -1,9 +1,6 @@
 import Type from '../dist'
 
 test(`getting types like "Type.Kind", not like "typeof Type['Kind']"`, () => {
-  const kind: Type.Kind = 'array'
-
-  console.log({
-    kind,
-  })
+  const kind: Type.Kind = 'null'
+  expect(kind)
 })
